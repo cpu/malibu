3c96a0153695ca1ea124bdf56dd42c71855db2c4
Daniel <daniel@binaryparadox.net>
2017-12-17 16:28:49 -0500

    WIP

---
+++ malibu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Malibu - automated integration testing for Streisand
+#
+
+# Set errexit option to exit immediately on any non-zero status return
+set -e
+
+echo -e "\n\033[38;5;255m\033[48;5;234m\033[1m  M A L I B U  \033[0m\n"
+
+git clone https://github.com/StreisandEffect/streisand.git
+
+cd streisand
