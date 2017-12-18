f48894ad90ea638b3803842315dfc9507d9b9406
Daniel <daniel@binaryparadox.net>
2017-12-18 17:06:31 -0500

    WIP 0.7

--- malibu.sh
+++ malibu.sh
@@ -11,3 +11,9 @@ echo -e "\n\033[38;5;255m\033[48;5;234m\033[1m  M A L I B U  \033[0m\n"
 git clone https://github.com/StreisandEffect/streisand.git
 
 ansible-playbook playbooks/integration-test.yml
+
+pushd streisand
+  deploy/streisand-new-cloud-server.sh --provider linode --site-config /tmp/travis-linode-site.yml
+  cat generated-docs/gateway-password.txt
+popd
+
