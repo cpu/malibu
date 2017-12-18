41be87d68403147289546f9a8dafe5b3a76105cf
Daniel <daniel@binaryparadox.net>
2017-12-18 17:11:41 -0500

    WIP 0.8

--- malibu.sh
+++ malibu.sh
@@ -8,8 +8,6 @@ set -e
 
 echo -e "\n\033[38;5;255m\033[48;5;234m\033[1m  M A L I B U  \033[0m\n"
 
-git clone https://github.com/StreisandEffect/streisand.git
-
 ansible-playbook playbooks/integration-test.yml
 
 pushd streisand
