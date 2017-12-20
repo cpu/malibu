00bd997ec9c1ded9d24e8baf0196a4e86da8e600
Daniel <daniel@binaryparadox.net>
2017-12-20 18:42:01 -0500

    WIP 0.17

--- malibu.sh
+++ malibu.sh
@@ -6,6 +6,18 @@
 # Set errexit option to exit immediately on any non-zero status return
 set -e
 
+cleanup() {
+  # Capture the return status previous to the cleanup
+  returnStatus=$?
+  # Run a playbook to delete created instances
+  ansible-playbook -e "server_id=$server_id" playbooks/cleanup.yml
+  # Exit with the captured status
+  exit $rv
+}
+
+# Ensure cleanup is called on exit
+trap "cleanup" INT TERM EXIT
+
 echo -e "\n\033[38;5;255m\033[48;5;234m\033[1m  M A L I B U  \033[0m\n"
 
 # Run a playbook to get set up for integration tests
@@ -19,8 +31,6 @@ pushd streisand
   server_id=$(jq .id integration-data/linode.metadata.json)
 
   # Run the integration tests against the Linode instance
-  ansible-playbook -i ../inventory -e "streisand_ip=$server_ip" playbooks/test-client.yml
+  ansible-playbook -vvv -i ../inventory -e "streisand_ip=$server_ip" playbooks/test-client.yml
 popd
 
-# Run a playbook to delete the instances created above
-ansible-playbook -e "server_id=$server_id" playbooks/cleanup.yml
